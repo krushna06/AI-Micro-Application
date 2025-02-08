4940623e71ab63a8c9db6a792504a92a0a07f96c
krushna06 <krushnavyas06@gmail.com>
2025-02-08 19:53:12 +0530

    init

---
+++ app/src/main/java/com/nostep/aimicrodegree/MainActivity.java
@@ -0,0 +1,23 @@
+package com.nostep.aimicrodegree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWeb = findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("https://aimicrodegree.org/login");
+    }
+}
